389104e6437497feab7a577958c8da0f83468d0f
Samuel <118542381+pewiebe@users.noreply.github.com>
2023-04-13 12:33:34 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-const horas = document.getElementById('horas');
+const horas = document.getElementById('horas'); //VARIAVEL TIPO TEMPO "CONST" PODE SER UMA VARIAVEL PARA CHAMAR O ELEMENTO EM QUE VC CRIOU NO INDEX
+                                                //DOCUMENT = document representa a página da web atual em que o código está sendo executado. Ele fornece uma interface para interagir com o conteúdo da página, como elementos HTML, estilos, eventos e outras propriedades
+                                                //document.GetelementByID = pega elemento pelo Id
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(function time (){
-    let dateToday = new Date();
+const relogio = setInterval(function time (){ // "CONST" variavel para o "RELOGIO"
+    // setInterval = Função tempo OU função
+    let dateToday = new Date(); // Let Objeto "algo" variáveis que vc pode criar 
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
